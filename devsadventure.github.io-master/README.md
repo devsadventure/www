3fe1b433ead0f348cb89b4dff65e480041f92bb7
devsadventure <margaret.the.adventurer@gmail.com>
2017-11-29 12:32:57 +0900

    폴더 삭제 및 디렉터리 초기화

--- devsadventure.github.io-master/README.md
+++
@@ -1,3 +0,0 @@
-# devsadventure.github.io
-
-## 과제를 수정 및 업로드하는 공간입니다.
--- devsadventure.github.io-master/rwd/menu-tab.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.menu-bar').click(function(e) {
-        e.preventDefault();
-        $(this).toggleClass("after");
-    });
-});
